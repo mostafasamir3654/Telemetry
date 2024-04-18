ed49b7f43e7e4735821b45340e73537e13d94720
Mostafa Mousa Ejada Systems Ltd <mmousa@ejada.com>
2024-04-18 14:55:52 +0200

    add matrices

--- Program.cs
+++ Program.cs
@@ -1,31 +1,69 @@
+using System.Diagnostics.Metrics;
+using System.Diagnostics;
 using System.Globalization;
+using System.Xml.Linq;
+
+var meter = new Meter("MyApplication");
+
+var counter = meter.CreateCounter<int>("Requests");
+var histogram = meter.CreateHistogram<float>("RequestDuration", unit: "ms");
+meter.CreateObservableGauge("ThreadCount", () => new[] { new Measurement<int>(ThreadPool.ThreadCount) });
+
+var httpClient = new HttpClient();
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 var logger = app.Logger;
 
+var activitySource = new ActivitySource("SampleActivitySource");
+
 int RollDice()
 {
     return Random.Shared.Next(1, 7);
 }
 
-string HandleRollDice(string? player)
+async Task<string> HandleRollDiceAsync(string? player)
 {
     var result = RollDice();
 
     if (string.IsNullOrEmpty(player))
     {
-        logger.LogInformation("Anonymous player is rolling the dice: {result}", result);
+        logger?.LogInformation("Anonymous player is rolling the dice: {result}", result);
     }
     else
     {
-        logger.LogInformation("{player} is rolling the dice: {result}", player, result);
+        logger?.LogInformation("{player} is rolling the dice: {result}", player, result);
     }
+
+    // The sampleActivity is automatically linked to the parent activity (the one from
+    // ASP.NET Core in this case).
+    // You can get the current activity using Activity.Current.
+    using (var sampleActivity = activitySource?.StartActivity("Sample", ActivityKind.Server))
+    {
+        // note that "sampleActivity" can be null here if nobody listen events generated
+        // by the "SampleActivitySource" activity source.
+        sampleActivity?.AddTag("Name", player);
+        sampleActivity?.AddBaggage("SampleContext", player);
+
+        // Simulate a long running operation
+        await Task.Delay(1000);
+    }
+
+    counter.Add(1, KeyValuePair.Create<string, object?>("name", player));
+    var stopwatch = Stopwatch.StartNew();
+    await httpClient.GetStringAsync("https://www.meziantou.net");
+
+    // Measure the duration in ms of requests and includes the host in the tags
+    histogram.Record(stopwatch.ElapsedMilliseconds,
+        tag: KeyValuePair.Create<string, object?>("Host", "www.meziantou.net"));
+
 
     return result.ToString(CultureInfo.InvariantCulture);
 }
 
-app.MapGet("/rolldice/{player?}", HandleRollDice);
+app.MapGet("/rolldice/{player?}", HandleRollDiceAsync);
 
+
+System.Console.WriteLine(System.Security.Principal.WindowsIdentity.GetCurrent().Name);
 app.Run();
